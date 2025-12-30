88a335f384922d07dffe97f536d1d0d47ed27973
Jayden <jayden.rerimassie@gmail.com>
2025-12-30 16:14:56 +0100

    Scripts folder met stored procedure voor database toegevoegd

---
+++ API/Scripts/01_StoredProcedures.sql
@@ -0,0 +1,35 @@
+﻿
+-- voer dit script uit in je eigen DB 
+-- (belangrijk voor de integriteit van Status property voor Reserveringen
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE type = 'P' AND name = 'sp_UpdateReservationStatuses')
+BEGIN
+    EXEC('
+    CREATE PROCEDURE sp_UpdateReservationStatuses
+    AS
+    BEGIN
+        SET NOCOUNT ON;
+        
+        UPDATE Reservations 
+        SET Status = 
+            CASE 
+                WHEN GETDATE() < StartDate THEN ''Gereserveerd''
+                WHEN GETDATE() BETWEEN StartDate AND EndDate THEN ''Actief''
+                ELSE ''Verlopen''
+            END
+        WHERE Status != 
+            CASE 
+                WHEN GETDATE() < StartDate THEN ''Gereserveerd''
+                WHEN GETDATE() BETWEEN StartDate AND EndDate THEN ''Actief''
+                ELSE ''Verlopen''
+            END
+        
+        PRINT ''Reservation statuses updated: '' + CONVERT(VARCHAR, @@ROWCOUNT) + '' rows affected'';
+    END
+    ')
+    PRINT 'Stored procedure sp_UpdateReservationStatuses created';
+END
+ELSE
+BEGIN
+    PRINT 'Stored procedure sp_UpdateReservationStatuses already exists';
+END
+GO
